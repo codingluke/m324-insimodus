8caf11ff07787b866a23aededbb34275e03b436c
Yannick Müller <mail@ynnk.dev>
2024-11-25 08:57:23 +0100

    chore: Setup Project (#23)

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterView />
+</template>
+
+<script lang="ts">
+export default {
+  
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue,jsx,tsx}", "./src/*.{html,js,vue,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
